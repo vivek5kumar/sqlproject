f79e4978988c973d39fe937cdee6c37dbd7a4998
vivek5kumar <vivek.yadav@sapple.co.in>
2022-10-14 17:00:32 +0530

    add file

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:get/get.dart';
 import 'dbmanager.dart';
 
@@ -25,174 +24,151 @@ class _HomePageState extends State<HomePage> {
     final size = MediaQuery.of(context).size;
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          title: const Text("Sqilite Demo"),
-        ),
-        body: ListView(
-          children: [
-            Form(
-                key: _formKey,
-                child: Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        controller: _nameController,
-                        autovalidateMode: AutovalidateMode.onUserInteraction,
-                        validator: (val) =>
-                            val!.isNotEmpty ? null : "Name should not be empty",
-                        decoration: InputDecoration(
-                            // contentPadding:
-                            //     const EdgeInsets.symmetric(vertical: 5.0),
-                            labelText: "Name",
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10))),
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      TextFormField(
-                        controller: _courseController,
-                        autovalidateMode: AutovalidateMode.onUserInteraction,
-                        validator: (val) => val!.isNotEmpty
-                            ? null
-                            : "Course should not be empty",
-                        decoration: InputDecoration(
-                            // contentPadding:
-                            //     const EdgeInsets.symmetric(vertical: 5.0),
-                            labelText: "Course",
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10))),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      SizedBox(
-                        width: size.width * 0.9,
-                        child: ElevatedButton(
-                            onPressed: () {
-                              // dbmanager.deleteTable();
-                              _submitStudent(context);
-                            },
-                            child: const Text("Submit")),
-                      ),
-                      FutureBuilder(
-                          future: dbmanager.getStudentList(),
-                          builder: (context, snapshot) {
-                            if (snapshot.hasData) {
-                              studList = snapshot.data!;
-                              studList.sort((a, b) => a.name!
-                                  .toLowerCase()
-                                  .compareTo(b.name!.toLowerCase()));
-                              return SizedBox(
-                                height: size.height * 0.6,
-                                child: AnimationLimiter(
-                                  child: studList.isNotEmpty
-                                      ? ListView.builder(
-                                          scrollDirection: Axis.vertical,
-                                          // shrinkWrap: true,
-                                          itemCount: studList == null
-                                              ? 0
-                                              : studList.length,
-                                          itemBuilder: (context, i) {
-                                            Student st = studList[i];
-                                            return AnimationConfiguration
-                                                .staggeredList(
-                                              position: i,
-                                              duration:
-                                                  const Duration(seconds: 2),
-                                              child: SlideAnimation(
-                                                // verticalOffset: 50.0,
-                                                child: FadeInAnimation(
-                                                  child: Card(
-                                                    child: Padding(
-                                                      padding:
-                                                          const EdgeInsets.all(
-                                                              8.0),
-                                                      child: Row(
-                                                        mainAxisAlignment:
-                                                            MainAxisAlignment
-                                                                .spaceAround,
-                                                        children: [
-                                                          SizedBox(
-                                                            width: size.width *
-                                                                0.5,
-                                                            child: Column(
-                                                              crossAxisAlignment:
-                                                                  CrossAxisAlignment
-                                                                      .start,
-                                                              children: [
-                                                                // Text(
-                                                                //     "Id: ${st.id.toString()}"),
-                                                                Text(
-                                                                    "Name: ${st.name.toString()}"),
-                                                                Text(
-                                                                    "Course: ${st.course.toString()}"),
-                                                              ],
-                                                            ),
-                                                          ),
-                                                          SizedBox(
-                                                            width: 20,
-                                                            child: InkWell(
-                                                                onTap: () {
-                                                                  _nameController
-                                                                          .text =
-                                                                      st.name
-                                                                          .toString();
-                                                                  _courseController
-                                                                          .text =
-                                                                      st.course
-                                                                          .toString();
-                                                                  student = st;
-                                                                  updateIndex =
-                                                                      i;
-                                                                },
-                                                                child:
-                                                                    const Icon(
-                                                                  Icons.edit,
-                                                                  color: Colors
-                                                                      .blue,
-                                                                )),
-                                                          ),
-                                                          SizedBox(
-                                                            width: 20,
-                                                            child: InkWell(
-                                                                onTap: () {
-                                                                  conformationAlert(
-                                                                      st, i);
-                                                                  // Get.back();
-                                                                },
-                                                                child:
-                                                                    const Icon(
-                                                                  Icons.delete,
-                                                                  color: Colors
-                                                                      .red,
-                                                                )),
-                                                          )
-                                                        ],
-                                                      ),
-                                                    ),
-                                                  ),
+          appBar: AppBar(
+            title: const Text("Sqilite Demo"),
+          ),
+          body: CustomScrollView(
+            slivers: [
+              SliverAppBar(
+                elevation: 2,
+                toolbarHeight: size.height * 0.37,
+                backgroundColor: Colors.white,
+                pinned: true,
+                title: Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          controller: _nameController,
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          validator: (val) => val!.isNotEmpty
+                              ? null
+                              : "Name should not be empty",
+                          decoration: InputDecoration(
+                              // contentPadding:
+                              //     const EdgeInsets.symmetric(vertical: 5.0),
+                              labelText: "Name",
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10))),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        TextFormField(
+                          controller: _courseController,
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          validator: (val) => val!.isNotEmpty
+                              ? null
+                              : "Course should not be empty",
+                          decoration: InputDecoration(
+                              // contentPadding:
+                              //     const EdgeInsets.symmetric(vertical: 5.0),
+                              labelText: "Course",
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10))),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        SizedBox(
+                          width: size.width * 0.9,
+                          child: ElevatedButton(
+                              onPressed: () {
+                                // dbmanager.deleteTable();
+                                _submitStudent(context);
+                              },
+                              child: const Text("Submit")),
+                        ),
+                      ],
+                    )),
+              ),
+              FutureBuilder(
+                  future: dbmanager.getStudentList(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      studList = snapshot.data!;
+                      studList.sort((a, b) => a.name!
+                          .toLowerCase()
+                          .compareTo(b.name!.toLowerCase()));
+                      return studList.isNotEmpty
+                          ? SliverList(
+                              delegate: SliverChildBuilderDelegate(
+                                childCount:
+                                    studList == null ? 0 : studList.length,
+                                (BuildContext context, i) {
+                                  Student st = studList[i];
+
+                                  return SizedBox(
+                                    height: size.height * 0.09,
+                                    child: Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 15.0),
+                                      child: Card(
+                                        child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceAround,
+                                          children: [
+                                            Center(
+                                              child: SizedBox(
+                                                width: size.width * 0.5,
+                                                child: Column(
+                                                  crossAxisAlignment:
+                                                      CrossAxisAlignment.start,
+                                                  children: [
+                                                    Text(
+                                                        "Name :${st.name.toString()}"),
+                                                    Text(
+                                                        "Course :${st.course.toString()}"),
+                                                  ],
                                                 ),
                                               ),
-                                            );
-                                          })
-                                      : const Center(child: Text("No Data")),
-                                ),
-                              );
-                            }
-                            return const Center(
-                              child: CircularProgressIndicator(
-                                color: Colors.red,
+                                            ),
+                                            SizedBox(
+                                              width: 20,
+                                              child: InkWell(
+                                                  onTap: () {
+                                                    _nameController.text =
+                                                        st.name.toString();
+                                                    _courseController.text =
+                                                        st.course.toString();
+                                                    student = st;
+                                                    updateIndex = i;
+                                                  },
+                                                  child: const Icon(
+                                                    Icons.edit,
+                                                    color: Colors.grey,
+                                                  )),
+                                            ),
+                                            SizedBox(
+                                              width: 20,
+                                              child: InkWell(
+                                                  onTap: () {
+                                                    conformationAlert(st, i);
+                                                    // Get.back();
+                                                  },
+                                                  child: const Icon(
+                                                    Icons.delete,
+                                                    color: Colors.red,
+                                                  )),
+                                            )
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  );
+                                },
                               ),
-                            );
-                          })
-                    ],
-                  ),
-                ))
-          ],
-        ),
-      ),
+                            )
+                          : const SliverToBoxAdapter(child: Text("No data "));
+                    }
+                    return const SliverToBoxAdapter(
+                      child: CircularProgressIndicator(
+                        color: Colors.red,
+                      ),
+                    );
+                  })
+            ],
+          )),
     );
   }
 
@@ -209,7 +185,7 @@ class _HomePageState extends State<HomePage> {
                 //     a.name!.toLowerCase().compareTo(b.name!.toLowerCase())),
                 _nameController.clear(),
                 _courseController.clear(),
-                print("student added to db ${id}"),
+                // print("student added to db ${id}"),
               });
         } else {
           student!.name = _nameController.text;
@@ -242,6 +218,7 @@ class _HomePageState extends State<HomePage> {
                   child: const Text("No")),
               TextButton(
                   onPressed: () {
+                    // Get.snackbar("Success", "One itme delete ${st.id}");
                     dbmanager.deleteStudent(st.id.toString());
                     setState(() {
                       studList.removeAt(i);
